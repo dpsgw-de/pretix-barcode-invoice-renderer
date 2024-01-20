aaaed166398203c108d4d74c0bf19fcf95f4d0bf
Fabian Lipp <fabian.lipp@gmx.de>
2024-01-20 17:49:47 +0100

    Add overloaded method for metadata and some debug

--- pretix_barcode_invoice_renderer/invoice.py
+++ pretix_barcode_invoice_renderer/invoice.py
@@ -1,4 +1,5 @@
 from django.utils.translation import pgettext
+from django.utils.formats import date_format
 
 from pretix.base.invoice import BaseReportlabInvoiceRenderer
 from pretix.base.invoice import Modern1Renderer
@@ -6,9 +7,19 @@ from pretix.base.invoice import Modern1Renderer
 from reportlab.graphics import barcode
 from reportlab.graphics.barcode import code128
 from reportlab.lib.units import mm
+from reportlab.lib.styles import ParagraphStyle, StyleSheet1
 from reportlab.pdfgen import canvas
 from reportlab.pdfgen.canvas import Canvas
+from reportlab.platypus import Paragraph
 
+from reportlab.lib import colors, pagesizes
+from reportlab.lib.enums import TA_CENTER, TA_LEFT, TA_RIGHT
+from reportlab.lib.styles import ParagraphStyle, StyleSheet1
+from reportlab.lib.units import mm
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.pdfmetrics import stringWidth
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import (
     BaseDocTemplate, Flowable, Frame, KeepTogether, NextPageTemplate,
     PageTemplate, Paragraph, Spacer, Table, TableStyle,
@@ -16,12 +27,9 @@ from reportlab.platypus import (
 
 import pprint
 
-
-
 class MyInvoiceRenderer(Modern1Renderer):
     identifier = 'barcode-dpsg'
     verbose_name = pgettext('invoice', 'Barcode Invoice Renderer (DPSG Würzburg)')
-    #logo_left = 0
 
     def _on_first_page(self, canvas: Canvas, doc):
         Modern1Renderer._on_first_page(self, canvas, doc)
@@ -30,35 +38,92 @@ class MyInvoiceRenderer(Modern1Renderer):
         canvas.restoreState()
 
     def _draw_barcode(self, canvas: Canvas):
-        barcode1=code128.Code128(self.invoice.number,barWidth=0.5*mm,barHeight=20*mm)
-        #canvas.translate(100*mm, 100*mm)
-        #barcode.canv = canvas
-        #barcode.draw()
-        #del barcode.canv
-        #barcode2 = barcode.createBarcodeDrawing("Code128", value=self.invoice.number, humanReadable=True,barWidth=0.5*mm,barHeight=20*mm)
-        barcode2 = barcode.createBarcodeDrawing("Code128", value="3802023AR12345", humanReadable=True, barWidth=0.3*mm, barHeight=18.5*mm, quiet=False)
-        posX = self.pagesize[0] - barcode1.width - self.right_margin
-        posX2 = self.pagesize[0] - barcode2.width - self.right_margin
-        posY = self.pagesize[1] - 90*mm  # Labels (invoice number, invoice date) etc. are at 100*mm from top
-        pprint.pprint(barcode2)
-        barcode1.drawOn(canvas,posX,posX)
-        barcode2.drawOn(canvas,posX2,posY)
-        canvas.circle(posX,posX, 5*mm, stroke=1, fill=1)
+        inv_barcode = barcode.createBarcodeDrawing("Code128", value="38020" + self.invoice.number, humanReadable=True, barWidth=0.31*mm, barHeight=18.5*mm, quiet=False)
+        posX = self.pagesize[0] - inv_barcode.width - self.right_margin
+        posY = self.pagesize[1] - 95*mm  # Labels for metadata (invoice number, invoice date) etc. are at 100*mm from top
+        inv_barcode.drawOn(canvas,posX,posY)
         canvas.circle(self.pagesize[0],posY, 2*mm, stroke=1, fill=1)
         canvas.circle(self.pagesize[0]-self.right_margin,posY, 2*mm, stroke=1, fill=1)
-        canvas.circle(self.pagesize[0]-self.right_margin-barcode2.width,posY, 2*mm, stroke=1, fill=1)
-        print("Hallo Welt 2")
-        debug_text = ("barcode1: " + str(barcode1.width) + " x " + str(barcode1.height) + ", <br />barcode2: " + str(barcode2.width) + " x " \
-                + str(barcode2.height) + "<br />mm: " + str(mm) + "<br />" + "barcode2 (mm): " + str(barcode2.width / mm) + " x " + str(barcode2.height / mm))
-        #debug_text = "Hallo Welt als Paragraph"
-        print(debug_text)
+        canvas.circle(self.pagesize[0]-self.right_margin-inv_barcode.width,posY, 2*mm, stroke=1, fill=1)
+        debug_text = ("inv_barcode: " + str(inv_barcode.width) + " x " \
+                + str(inv_barcode.height) + "<br />mm: " + str(mm) + "<br />" + "inv_barcode: " + str(inv_barcode.width / mm) + "mm x " + str(inv_barcode.height / mm) + "mm")
         p = Paragraph(debug_text, style=self.stylesheet['Sender'])
         p.wrapOn(canvas, 75*mm, 100*mm)
         p.drawOn(canvas, 25*mm, 55*mm)
-        p = Paragraph("Code version 13", style=self.stylesheet['Sender'])
+        p = Paragraph("Code version 18", style=self.stylesheet['Sender'])
         p.wrapOn(canvas, 100*mm, 100*mm)
-        p.drawOn(canvas, 25*mm, 15*mm)
-        p = Paragraph("This paragraph is at (posX,posX)", style=self.stylesheet['Sender'])
+        p.drawOn(canvas, 25*mm, 40*mm)
+        p = Paragraph("event: " + pprint.pformat(self.event.meta_data), style=self.stylesheet['Sender'])
         p.wrapOn(canvas, 100*mm, 100*mm)
-        p.drawOn(canvas, posX, posX)
+        p.drawOn(canvas, 25*mm, 35*mm)
 
+    # This method is mostly copied from Modern1Renderer, only adds an additional field for metadata
+    def _draw_metadata(self, canvas):
+        # Draws the "invoice number -- date" line. This has gotten a little more complicated since we
+        # encountered some events with very long invoice numbers. In this case, we automatically reduce
+        # the font size until it fits.
+        begin_top = 100 * mm
+
+        def _draw(label, value, value_size, x, width, bold=False, sublabel=None):
+            if canvas.stringWidth(value, self.font_regular, value_size) > width and value_size > 6:
+                return False
+            textobject = canvas.beginText(x, self.pagesize[1] - begin_top)
+            textobject.setFont(self.font_regular, 8)
+            textobject.textLine(self._normalize(label))
+            textobject.moveCursor(0, 5)
+            textobject.setFont(self.font_bold if bold else self.font_regular, value_size)
+            textobject.textLine(self._normalize(value))
+
+            if sublabel:
+                textobject.moveCursor(0, 1)
+                textobject.setFont(self.font_regular, 8)
+                textobject.textLine(self._normalize(sublabel))
+
+            return textobject
+
+        value_size = 10
+        while value_size >= 5:
+            if self.event.settings.invoice_renderer_highlight_order_code:
+                kwargs = dict(bold=True, sublabel=pgettext('invoice', '(Please quote at all times.)'))
+            else:
+                kwargs = {}
+            objects = [
+                _draw(pgettext('invoice', 'Order code'), self.invoice.order.full_code, value_size, self.left_margin, 45 * mm, **kwargs)
+            ]
+
+            p = Paragraph(
+                self._normalize(date_format(self.invoice.date, "DATE_FORMAT")),
+                style=ParagraphStyle(name=f'Normal{value_size}', fontName=self.font_regular, fontSize=value_size, leading=value_size * 1.2)
+            )
+            w = stringWidth(p.text, p.frags[0].fontName, p.frags[0].fontSize)
+            p.wrapOn(canvas, w, 15 * mm)
+            date_x = self.pagesize[0] - w - self.right_margin
+
+            if self.invoice.is_cancellation:
+                objects += [
+                    _draw(pgettext('invoice', 'Cancellation number'), self.invoice.number,
+                          value_size, self.left_margin + 50 * mm, 45 * mm),
+                    _draw(pgettext('invoice', 'Original invoice'), self.invoice.refers.number,
+                          value_size, self.left_margin + 100 * mm, date_x - self.left_margin - 100 * mm - 5 * mm),
+                ]
+            else:
+                objects += [
+                    _draw(pgettext('invoice', 'Invoice number'), self.invoice.number,
+                          value_size, self.left_margin + 70 * mm, date_x - self.left_margin - 70 * mm - 5 * mm),
+                ]
+
+            if all(objects):
+                for o in objects:
+                    canvas.drawText(o)
+                break
+            value_size -= 1
+
+        p.drawOn(canvas, date_x, self.pagesize[1] - begin_top - 10 - 6)
+
+        textobject = canvas.beginText(date_x, self.pagesize[1] - begin_top)
+        textobject.setFont(self.font_regular, 8)
+        if self.invoice.is_cancellation:
+            textobject.textLine(self._normalize(pgettext('invoice', 'Cancellation date')))
+        else:
+            textobject.textLine(self._normalize(pgettext('invoice', 'Invoice date')))
+        canvas.drawText(textobject)
